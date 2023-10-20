de9f822c8c0e9a5cb8756776fc189ebd5dd87c5c
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-10-20 10:37:28 -0300

    nuevos archivos: .env modificados:     bun.lockb modificados:     package.json nuevos archivos: src/app/auth/callback/route.ts nuevos archivos: src/app/auth/login/route.ts nuevos archivos: src/app/auth/logout/route.ts nuevos archivos: src/app/auth/sign-up/route.ts modificados:     src/app/globals.css nuevos archivos: src/app/login/page.tsx modificados:     src/app/page.tsx nuevos archivos: src/lib/database.types.ts nuevos archivos: src/middleware.ts

---
+++ src/app/login/page.tsx
@@ -0,0 +1,25 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function LoginPage() {
+    const supabase = createServerComponentClient({ cookies });
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+        return redirect("/");
+    }
+
+    return (
+        <div>
+            <form action="/auth/login" method="POST">
+                <input type="text" placeholder="email" name="email" />
+                <input type="password" placeholder="password" name="password" />
+                <button type="submit">Login</button>
+                <button formAction="/auth/sign-up">Sign Up</button>
+            </form>
+        </div>
+    );
+}
