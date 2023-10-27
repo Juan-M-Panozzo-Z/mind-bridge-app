e839ebfd531cef40380cca026b96226da08029b7
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-10-27 10:58:12 -0300

    modificados:     src/components/Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,12 +17,15 @@ import {
 } from "./ui/sheet";
 import { Box, Flex, Text } from "@radix-ui/themes";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Sidebar() {
     return (
         <Sheet>
             <SheetTrigger>
-                <PanelLeftOpen className="mr-4" />
+                <Button size={"icon"} variant={"ghost"} className="mr-4">
+                    <PanelLeftOpen/>
+                </Button>
             </SheetTrigger>
             <SheetContent side={"left"} className="w-56">
                 <SheetHeader>
@@ -35,28 +38,20 @@ export default function Sidebar() {
                 </SheetHeader>
                 <Box className="grid gap-4 py-8">
                     <Link href="/" className="flex items-center">
-                        <HomeIcon className="mr-2"/>
-                        <span className="font-normal text-xs">
-                            Inicio
-                        </span>
+                        <HomeIcon className="mr-2" />
+                        <span className="font-normal text-xs">Inicio</span>
                     </Link>
                     <Link href="/" className="flex items-center">
-                        <User className="mr-2"/>
-                        <span className="font-normal text-xs">
-                            Pacientes
-                        </span>
+                        <User className="mr-2" />
+                        <span className="font-normal text-xs">Pacientes</span>
                     </Link>
                     <Link href="/" className="flex items-center">
-                        <CircleDashed className="mr-2"/>
-                        <span className="font-normal text-xs">
-                            Circulos
-                        </span>
+                        <CircleDashed className="mr-2" />
+                        <span className="font-normal text-xs">Circulos</span>
                     </Link>
                     <Link href="/" className="flex items-center">
-                        <Cross className="mr-2"/>
-                        <span className="font-normal text-xs">
-                            Evoluciones
-                        </span>
+                        <Cross className="mr-2" />
+                        <span className="font-normal text-xs">Evoluciones</span>
                     </Link>
                 </Box>
             </SheetContent>
