75caf6ab7cdc6e8b54b051c1b580a7aa63e05e69
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-10-31 13:52:56 -0300

    modificados:     src/app/(dashboard)/circles/components/CreateCircle.tsx 	modificados:     src/app/(dashboard)/circles/components/ListCircles.tsx 	borrados:        src/app/(dashboard)/loading.tsx 	modificados:     src/components/ui/section.tsx

--- src/app/(dashboard)/circles/components/CreateCircle.tsx
+++ src/app/(dashboard)/circles/components/CreateCircle.tsx
@@ -1,6 +1,8 @@
 import { Box, Container, Flex, Text } from "@radix-ui/themes";
 import SectionComponent from "@/components/ui/section";
 import Create from "./FormCreateCircle";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 export default async function CreateCircle() {
     const title = "Crear círculo";
@@ -8,20 +10,22 @@ export default async function CreateCircle() {
         "Aquí podrás crear un circulo de colegas junto a las familias de un paciente";
 
     return (
-        <SectionComponent>
-            <Container size={"4"} className="px-4">
-                <Box className="flex justify-between items-center">
-                    <Flex direction={"column"}>
-                        <Text size={"2"}>{title}</Text>
-                        <Text size={"1"} className="text-foreground/70">
-                            {description}
-                        </Text>
-                    </Flex>
-                    <Box>
-                        <Create />
+        <Suspense fallback={<Skeleton className="min-h-[80px]" />}>
+            <SectionComponent>
+                <Container size={"4"} className="px-4">
+                    <Box className="flex justify-between items-center">
+                        <Flex direction={"column"}>
+                            <Text size={"2"}>{title}</Text>
+                            <Text size={"1"} className="text-foreground/70">
+                                {description}
+                            </Text>
+                        </Flex>
+                        <Box>
+                            <Create />
+                        </Box>
                     </Box>
-                </Box>
-            </Container>
-        </SectionComponent>
+                </Container>
+            </SectionComponent>
+        </Suspense>
     );
 }
--- src/app/(dashboard)/circles/components/ListCircles.tsx
+++ src/app/(dashboard)/circles/components/ListCircles.tsx
@@ -14,6 +14,8 @@ import {
 import { Button } from "@/components/ui/button";
 
 import { Trash2 } from "lucide-react";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function ListCircles() {
     const title = "Círculos";
@@ -22,64 +24,66 @@ export default async function ListCircles() {
 
     const circles = await fetchCirclesByOwner();
     return (
-        <SectionComponent>
-            <Container size={"4"} className="px-4">
-                <Flex direction={"column"}>
-                    <Text size={"2"}>{title}</Text>
-                    <Text size={"1"} className="text-foreground/70">
-                        {description}
-                    </Text>
-                </Flex>
-                {circles.length > 0 ? (
-                    <Table className="mt-4">
-                        <TableCaption>
-                            Circulos creados por el usuario
-                        </TableCaption>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>Nombre</TableHead>
-                                <TableHead>Fecha de creación</TableHead>
-                                <TableHead className="text-right">
-                                    Acciones
-                                </TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {circles.map((circle) => (
-                                <TableRow key={circle.id}>
-                                    <TableCell>{circle.name}</TableCell>
-                                    <TableCell>
-                                        {
-                                            circle.created_at
-                                                .toString()
-                                                .split("T")[0]
-                                        }
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <form
-                                            action={deleteCircle}
-                                            className="inline-block"
-                                        >
-                                            <input
-                                                type="hidden"
-                                                name="id"
-                                                value={circle.id}
-                                            />
-                                            <Button variant={"ghost"}>
-                                                <Trash2 />
-                                            </Button>
-                                        </form>
-                                    </TableCell>
+        <Suspense fallback={<Skeleton className="min-h-[150px]" />}>
+            <SectionComponent>
+                <Container size={"4"} className="px-4">
+                    <Flex direction={"column"}>
+                        <Text size={"2"}>{title}</Text>
+                        <Text size={"1"} className="text-foreground/70">
+                            {description}
+                        </Text>
+                    </Flex>
+                    {circles.length > 0 ? (
+                        <Table className="mt-4">
+                            <TableCaption>
+                                Circulos creados por el usuario
+                            </TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Nombre</TableHead>
+                                    <TableHead>Fecha de creación</TableHead>
+                                    <TableHead className="text-right">
+                                        Acciones
+                                    </TableHead>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                ) : (
-                    <Box className="flex justify-center items-center mt-4">
-                        <Text size={"1"}>No hay círculos creados</Text>
-                    </Box>
-                )}
-            </Container>
-        </SectionComponent>
+                            </TableHeader>
+                            <TableBody>
+                                {circles.map((circle) => (
+                                    <TableRow key={circle.id}>
+                                        <TableCell>{circle.name}</TableCell>
+                                        <TableCell>
+                                            {
+                                                circle.created_at
+                                                    .toString()
+                                                    .split("T")[0]
+                                            }
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <form
+                                                action={deleteCircle}
+                                                className="inline-block"
+                                            >
+                                                <input
+                                                    type="hidden"
+                                                    name="id"
+                                                    value={circle.id}
+                                                />
+                                                <Button variant={"ghost"}>
+                                                    <Trash2 />
+                                                </Button>
+                                            </form>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    ) : (
+                        <Box className="flex justify-center items-center mt-4">
+                            <Text size={"1"}>No hay círculos creados</Text>
+                        </Box>
+                    )}
+                </Container>
+            </SectionComponent>
+        </Suspense>
     );
 }
