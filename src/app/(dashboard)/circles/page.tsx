1878dcbdda606ffceee4d249285598a41679dadc
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-10-31 11:16:54 -0300

    modificados:     src/app/(dashboard)/circles/page.tsx 	modificados:     src/app/(dashboard)/circles/page.tsx

--- src/app/(dashboard)/circles/page.tsx
+++ src/app/(dashboard)/circles/page.tsx
@@ -5,16 +5,8 @@ import ListCircles from "./components/ListCircles";
 export default function CirclesPage() {
     return (
         <Section className="md:col-span-3 space-y-4">
-            <Text size={"2"}>
-                Estos componentes estan en desarrollo, no tener en cuenta la UI
-                preliminar
-            </Text>
-            <Box className="md:col-span-3">
-                <CreateCircle />
-            </Box>
-            <Box className="md:col-span-3">
-                <ListCircles />
-            </Box>
+            <CreateCircle />
+            <ListCircles />
         </Section>
     );
 }
