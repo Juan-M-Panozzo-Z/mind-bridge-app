33627b34495c0abbc37ff34b37bca159400e470f
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-11-08 20:16:43 -0300

    modified:   package-lock.json modified:   package.json new file:   src/app/(dashboard)/firststeps/page.tsx

---
+++ src/app/(dashboard)/firststeps/page.tsx
@@ -0,0 +1,35 @@
+import SectionComponent from "@/components/ui/section";
+import { Box, Container, Flex, Section } from "@radix-ui/themes";
+import { Button } from "@/components/ui/button";
+import { Cross, User } from "lucide-react";
+
+export default function FirstStepsPage() {
+    return (
+        <Section className="md:col-span-3 space-y-4">
+            <SectionComponent>
+                <Container size="4" className="px-4">
+                    <Box className="grid grid-cols-2 gap-2">
+                        <Flex
+                            gap={"2"}
+                            direction={"column"}
+                            align={"center"}
+                            className="border shadow rounded-xl p-4"
+                        >
+                            <Cross size={72} />
+                            <Button>Soy Profesional de la salud</Button>
+                        </Flex>
+                        <Flex
+                            gap={"2"}
+                            direction={"column"}
+                            align={"center"}
+                            className="border shadow rounded-xl p-4"
+                        >
+                            <User size={72} />
+                            <Button>Soy Paciente</Button>
+                        </Flex>
+                    </Box>
+                </Container>
+            </SectionComponent>
+        </Section>
+    );
+}
