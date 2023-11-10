cc0eb17b31180114466e1439c8111b31367705fc
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-11-10 10:53:28 -0300

    modificados:     src/app/(dashboard)/page.tsx

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,14 @@
 import GetProfile from "@/components/GetProfile";
 import Dashboard from "./profile/components/Dashboard";
+import { Flex, Section } from "@radix-ui/themes";
 
 export default async function Home() {
-
     return (
         <>
-        <GetProfile />
-        <Dashboard />
+            <GetProfile />
+            <Section size={"1"} className="md:col-span-3 gap-4 grid md:grid-cols-2 lg:grid-cols-3">
+            <Dashboard />
+            </Section>
         </>
-    )
+    );
 }
